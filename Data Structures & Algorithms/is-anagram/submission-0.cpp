0f56708ebef78c187dcf6ecce6630719a7727682
Utkarsh Yadav <utkarshdhana@gmail.com>
2026-08-18 16:53:32 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        int hash[26];
+        for(int i = 0; i < s.size(); i++) {
+            hash[s[i] - 'a']++;
+            hash[t[i] - 'a']--;
+        }
+        for(auto it : hash) {
+            if(it != 0) return false;
+        }
+        return true;
+    }
+};
