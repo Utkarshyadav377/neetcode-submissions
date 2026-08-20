a512b56e51925258320e9867ebab0780aea07426
Utkarsh Yadav <utkarshdhana@gmail.com>
2026-08-20 18:51:01 +0530

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    bool palindrome(string &s, int l, int r) {
+        while(l < r) {
+            if(s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+public:
+    bool validPalindrome(string s) {
+        int n = s.size();
+        int l = 0;
+        int r = n - 1;
+
+        while(l < r) {
+            if(s[l] != s[r]) {
+                return palindrome(s, l + 1, r) || palindrome(s, l, r - 1);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
